88c3996ff817cd078a5c00b90d0f122dd9f97037
alexanderboutinflegel_phl <alexander.boutinflegel@propellerhead.co.nz>
2019-05-30 12:01:27 +1200

    Removed the unused Request action

--- src/Hooks/SearchStatistics/Actions/index.ts
+++ src/Hooks/SearchStatistics/Actions/index.ts
@@ -1,16 +1,11 @@
-import { IRequestStatistics, RequestStatisticsAction } from "./RequestStatistics";
 import { IFetchStatistics, FetchStatisticsAction } from "./FetchStatistics";
 import { PoolCombination } from "../../../Models/PoolCombination";
 
 // Action interfaces
-export type StatisticsApiActions = IRequestStatistics | IFetchStatistics;
+export type StatisticsApiActions = IFetchStatistics;
 
 // Action creators
 export const fetchStatisticsAction = (combination: PoolCombination): IFetchStatistics => ({
 	poolCombination: combination,
 	type: FetchStatisticsAction
-});
-
-export const requestAction = (): IRequestStatistics => ({
-	type: RequestStatisticsAction
 });
