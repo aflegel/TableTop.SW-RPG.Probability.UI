88425ca0401bed636f26355d2859f1a50ccaad0d
Alexander Boutin Flegel <alexander.boutinflegel@propellerhead.co.nz>
2019-10-19 15:22:15 +1300

    Updated packages Removed the about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement } from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import { Layout } from "./Components/Layout";
-import { About } from "./Components/About";
 import { Statistics } from "./Components/Statistics";
 
 import "sw-rpg-icons/css/sw-rpg-icons.scss";
@@ -12,8 +11,6 @@ export const App = (): ReactElement =>
 		<Switch>
 			<Layout>
 				<Route exact path="/" component={Statistics} />
-				<Route path="/About" component={About} />
 			</Layout>
 		</Switch>
 	</HashRouter>;
-
