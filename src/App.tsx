62be5ba10a369b6dbc9ee1890d3756f043aafa20
Alexander Boutin Flegel <alex.g.flegel@gmail.com>
2021-03-16 14:30:43 +1300

    updated to react 17 (#24)
    fixed Function build errors
    removed "router" components
    Security fixes
    
    Co-authored-by: Alexander Boutin Flegel <alexander.boutinflegel@propellerhead.co.nz>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React, { ReactElement } from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
 import { Layout } from "./Components/Layout";
 import { Statistics } from "./Components/Statistics";
 
 import "sw-rpg-icons/css/sw-rpg-icons.scss";
 import "sw-rpg-icons/css/sw-rpg-colors.scss";
 
-export const App = (): ReactElement =>
-	<HashRouter>
-		<Switch>
-			<Layout>
-				<Route exact path="/" component={Statistics} />
-			</Layout>
-		</Switch>
-	</HashRouter>;
+export const App = (): ReactElement => (
+	<Layout>
+		<Statistics />
+	</Layout>
+);
