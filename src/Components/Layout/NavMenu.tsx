9d33bde67486760e40f52a56dbc3ec14d7bb9392
Alexander Boutin Flegel <alexander.boutinflegel@propellerhead.co.nz>
2021-03-22 10:56:20 +1300

    [usability] Fixed navbar colour issues Removed empty button from nav added healthcheck to rest tools

--- src/Components/Layout/NavMenu.tsx
+++ src/Components/Layout/NavMenu.tsx
@@ -1,10 +1,30 @@
 import React, { ChangeEvent, ReactElement, ReactNode, useContext } from "react";
-import { AppBar, Toolbar, Typography, Button, FormControl, InputLabel, MenuItem, Select, Grid } from "@material-ui/core";
-import { FormattedMessage } from "react-intl";
+import { AppBar, Toolbar, Typography, Button, FormControl, InputLabel, MenuItem, Select, Grid, makeStyles, Theme, createStyles, styled } from "@material-ui/core";
+import { FormattedMessage, useIntl } from "react-intl";
 import { IntlWrapperContext, Locales } from "../../Hooks/IntlWrapper";
 
+const useStyles = makeStyles((theme: Theme) => {
+	const selectColor = theme.palette.getContrastText(theme.palette.primary.main);
+
+	return createStyles({
+		formControl: {
+			minWidth: 120,
+		},
+		colorInherit: {
+			color: "inherit !important",
+		},
+		select: {
+			"&:before, &:after": {
+				borderColor: `${selectColor} !important`,
+			},
+		},
+	});
+});
+
 export const NavMenu = (): ReactElement => {
+	const intl = useIntl();
 	const intlWrapper = useContext(IntlWrapperContext);
+	const classes = useStyles();
 
 	const handleChange = (event: ChangeEvent<{ name?: string | undefined; value: any }>, child: ReactNode) => intlWrapper.update((event.target.value as Locales) || "en-us");
 
@@ -14,18 +34,25 @@ export const NavMenu = (): ReactElement => {
 				<Grid justify="space-between" container>
 					<Grid item>
 						<Typography variant="h6" color="inherit">
-							<Button color="inherit">
-								<FormattedMessage id="Title" />
-							</Button>
+							<FormattedMessage id="Title" />
 						</Typography>
 					</Grid>
 					<Grid item>
 						<Typography variant="h6" color="inherit">
-							<FormControl>
-								<InputLabel id="language">
-									<FormattedMessage id="Language" />
-								</InputLabel>
-								<Select labelId="language" onChange={handleChange} value={intlWrapper.current.locale}>
+							<FormControl className={classes.formControl}>
+								<Select
+									onChange={handleChange}
+									value={intlWrapper.current.locale}
+									className={[classes.select, classes.colorInherit].join(" ")}
+									inputProps={{
+										name: "size",
+										id: "size",
+										classes: {
+											icon: classes.colorInherit,
+										},
+										"aria-label": intl.formatMessage({ id: "Language" }),
+									}}
+								>
 									<MenuItem value="en-us">English</MenuItem>
 									<MenuItem value="jp">日本語</MenuItem>
 									<MenuItem value="mi">Te Reo</MenuItem>
